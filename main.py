80612689892d48fddc9cc712060846ca14159684
unknown <duy.luongduy16201@hcmut.edu.vn>
2025-12-21 15:59:51 +0700

    [dev] fix parametter for upgrade coverage percentage

--- main.py
+++ main.py
@@ -302,9 +302,9 @@ def load_case_from_json(file_path):
     return env, obstacles, start
 
 if __name__ == "__main__":
-    env, obstacles, start = load_case_from_json("testcase_4.json")
+    env, obstacles, start = load_case_from_json("testcase_5.json")
     path, G, env_free, covered = C_star_coverage(env, obstacles, start_pos=start,
-                                       sensor_range=5.0, cover_radius=1.5, d_s=5.0,
+                                       sensor_range=6.0, cover_radius=5.5, d_s=5.0,
                                        verbose=True)
     # ============================
     # Coverage metrics
